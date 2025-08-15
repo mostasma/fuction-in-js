14fcb7b0a09fa589ef6dd470e70a61822e9a5d05
mostasma <asmahasan35@gmail.com>
2025-08-15 09:23:06 +0200

    updated all feature

--- problem2.js
+++ problem2.js
@@ -1,1 +1,36 @@
- 
+ function calculateBill(rice, curry, drinks){
+    return  rice + curry + drinks;
+ }
+ console.log(calculateBill(12,39,49));
+
+
+
+
+ function isGreaterThenTen(num){
+    return num>10;
+ }
+ console.log(isGreaterThenTen(20));
+ console.log(isGreaterThenTen(10));
+
+
+
+ function isDivisibleBy13(num){
+    return num % 13===0;
+ }
+ console.log(isDivisibleBy13(26));
+ console.log(isDivisibleBy13(38));
+
+
+
+ function getStringLength(num){
+  return num.length;
+ }
+ console.log(getStringLength("Bangladeh"));
+ console.log(getStringLength([12,34,567,88,12,2,2,3]));
+
+
+ function sumThreeNumbers(num1, num2, num3){
+    return (num1+num2+num3)/3;
+
+ }
+ console.log(sumThreeNumbers(10,20,30));
